1d2d91b9e86ce8bc4798aea6f12156685c94c868
Alencea <67931836+Alencea@users.noreply.github.com>
2022-11-09 15:12:03 -0500

    Add files via upload

---
+++ MyLinkedListNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fishhook
+{
+    internal class MyLinkedListNode<T>
+    {
+        public T? Value { get; set; }
+        public MyLinkedListNode<T> next { get; set; }
+
+        public MyLinkedListNode(T element)
+        {
+            this.Value = element;
+        }
+    }
+}
