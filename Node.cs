70dc8010bedb88ebf50fbec4325b20f11c90e267
Alencea <67931836+Alencea@users.noreply.github.com>
2022-11-09 14:54:15 -0500

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryKioskSystem
+{
+    internal class Node<Book>
+    {
+        Book book;
+        public int self;
+        public int rightChild;
+        public int leftChild;
+    }
+}
