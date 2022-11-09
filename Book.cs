515c298dfab6fa53836f95c66288e564546771c7
Alencea <67931836+Alencea@users.noreply.github.com>
2022-11-09 14:30:58 -0500

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryKioskSystem
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public string Publiser { get; set; }
+
+        public void Print(string Title, string Author, int Pages, string Publisher) => Console.WriteLine(Title + ", " + Author + ", " + Pages + ", " + Publisher);
+    }
+
+    
+}
